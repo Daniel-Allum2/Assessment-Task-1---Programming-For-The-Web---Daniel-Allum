337f91e6fd1d264490726fb88f2860b148b65713
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-12-07 04:53:59 +0000

    python index

--- App.py
+++ App.py
@@ -5,3 +5,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = "secret-key"
 DB = "database.db"
+
+
+def index():
+    db = sqlite3.connect(DB)
+    db.row_factory = sqlite3.Row
+    movies = db.execute("SELECT * FROM Movies").fetchall()
+    db.close()
+    return render_template("index.html", movies=movies)
+
+
+app.run(debug=True, port=5000)
