4d3c7214281784b2fd1cd34727c37cb114321841
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-12-08 06:10:05 +0000

    Fixed website opening

--- App.py
+++ App.py
@@ -4,9 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "secret-key"
-DB = "database.db"
+DB = "DATABASE/Movie_App.db"
 
 
+@app.route("/")
 def index():
     db = sqlite3.connect(DB)
     db.row_factory = sqlite3.Row
